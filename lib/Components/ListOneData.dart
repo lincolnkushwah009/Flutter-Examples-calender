90ada67be4abdd107a871db0e8c35eec1a6b1800
Lincoln Kushwah <lincolnkushwah009@gmail.com>
2020-08-28 18:01:31 +0530

    list 1

--- lib/Components/ListOneData.dart
+++ lib/Components/ListOneData.dart
@@ -16,33 +16,64 @@ class _ListOneDataState extends State<ListOneData> {
         body: ListView(
 
           children: List.generate(40, (index) {
-            return  Padding(
-              padding: const EdgeInsets.all(8.0),
+            return   Padding(
+              padding: const EdgeInsets.fromLTRB(2, 5, 2, 15),
               child: Container(
                 decoration: BoxDecoration(
-                    borderRadius: new BorderRadius.circular(10.0),
-                    color: Colors.white
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 7.0,
+                    ),
+                  ],
+                  borderRadius: new BorderRadius.circular(10.0),
+                  color: Colors.white,
                 ),
                 height: MediaQuery.of(context).size.height * 0.1,
-                width: MediaQuery.of(context).size.width * 1,
+                width: MediaQuery.of(context).size.width * 0.9,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: <Widget>[
                     Container(
-                      width: MediaQuery.of(context).size.width * 0.2,
-                      child: Text("Text",style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),),
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      child: Text("a"),
                     ),
-
                     Container(
-                      width: MediaQuery.of(context).size.width * 0.2,
-                      child: Text("Photo",style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),),
+                      height: 50,
+                      color: Colors.black12,
+                      width: 4,
                     ),
-
                     Container(
-                      width: MediaQuery.of(context).size.width * 0.2,
-                      child: Text("Video",style: TextStyle(fontSize: 14,fontWeight: FontWeight.bold),),
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      child: Text("a"),
                     ),
-
+                    Container(
+                      height: 50,
+                      color: Colors.black12,
+                      width: 4,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      child: Text("a"),
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.black12,
+                      width: 4,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      child: Text("a"),
+                    ),
+                    Container(
+                      height: 50,
+                      color: Colors.black12,
+                      width: 4,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      child: Image.asset("assets/background.png"),
+                    ),
                   ],
                 ),
               ),
